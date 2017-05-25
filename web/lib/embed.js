91da0823e7382c1e7a22a977254a5f8ea015727a
Tomas Petricek <tomas@tomasp.net>
2017-05-25 02:44:53 +0100

    Guess video views

--- web/lib/embed.js
+++ web/lib/embed.js
@@ -21,13 +21,14 @@ function loadTheGamma() {
     map:{ "*":{"monaco":"vs/editor/editor.main"}}
   });
   require(["vs/editor/editor.main", "node_modules/thegamma-script/dist/thegamma.js"], function (_, g) {      
-    thegamma.forEach(function (id) {
+    thegamma.forEach(function (info) {
+      var id = info.id;
       var services = "https://thegamma-services.azurewebsites.net/";      
       var providers = 
         g.providers.createProviders({ 
           "worldbank": g.providers.rest(services + "worldbank"),
           "libraries": g.providers.library("node_modules/thegamma-script/dist/libraries.json"),
-          "shared": g.providers.rest("https://gallery-csv-service.azurewebsites.net/providers/listing"),
+          "shared": g.providers.rest("https://gallery-csv-service.azurewebsites.net/providers/listing", null, true),
           "olympics": g.providers.pivot(services + "pdata/olympics") });
         
       // Create context and setup error handler
@@ -42,19 +43,9 @@ function loadTheGamma() {
         document.getElementById("thegamma-" + id + "-errors").innerHTML = ul;
       });
       
-      // Get and run default code, setup update handler
-      var editor;
-      var code = document.getElementById(id + "-code").innerHTML;
-      ctx.evaluate(code, "thegamma-" + id + "-out");
-      document.getElementById("thegamma-" + id + "-update").onclick = function() {
-        ctx.evaluate(editor.getValue(), "thegamma-" + id + "-out");
-        closeDialog(id);
-        return false;
-      }
-
       // Specify options and create the editor
       var opts =
-        { height: document.getElementById("thegamma-" + id + "-sizer").clientHeight-130,
+        { height: document.getElementById("thegamma-" + id + "-sizer").clientHeight-250,
           width: document.getElementById("thegamma-" + id + "-sizer").clientWidth-20,
           monacoOptions: function(m) {
             m.fontFamily = "Inconsolata";
@@ -62,14 +53,53 @@ function loadTheGamma() {
             m.lineHeight = 20;
             m.lineNumbers = false;
           } };
-      editor = ctx.createEditor("thegamma-" + id + "-ed", code, opts);
+      var editor = ctx.createEditor("thegamma-" + id + "-ed", code, opts);
+      
+      // Set source code, evalate it, 
+      function setSource(code) {
+        ctx.evaluate(code, "thegamma-" + id + "-out");      
+        editor.setValue(code);
+        if (info.editors) {
+          ctx.check(code).then(function(res) {
+            if (res.wellTyped) {          
+              res.getEntities()
+                .filter(function(ent) { return ent.kind == "placeholder"; })
+                .forEach(function(place) {
+                  var html = "";
+                  var selected = place.getChildren()[0].getChildren()[1];
+                  place.getChildren()[0].getChildren()[0].type.members
+                    .filter(function(m) { return m != "preview"; })
+                    .forEach(function(m) {
+                      var sel = m == selected.name ? " selected" : "";
+                      html += "<option value='" + m + "'" + sel + ">" + m + "</option>";
+                    });
+                  var drop = document.getElementById(info.editors + "-" + place.name);
+                  drop.onchange = function() { 
+                    var newCode = code.substr(0, place.range.start) + "[" + place.name + ":'" + drop.value + "']" + code.substr(place.range.end + 1);
+                    setSource(newCode);
+                  };
+                  drop.innerHTML = html;
+                });
+            }
+          });
+        }
+      }
+      
+      // Get and run default code, setup update handler
+      var code = document.getElementById(id + "-code").innerHTML;      
+      setSource(code);
+      document.getElementById("thegamma-" + id + "-update").onclick = function() {
+        ctx.evaluate(editor.getValue(), "thegamma-" + id + "-out");
+        closeDialog(id);
+        return false;
+      }
     });
   });
 }
 
 function initTheGamma() {
-  thegamma.forEach(function(id) {
-    var el = document.getElementById(id);  
+  thegamma.forEach(function(info) {
+    var el = document.getElementById(info.id);  
     el.innerHTML = 
       ("<div class='thegamma-edit'><a href='javascript:openDialog(\"[ID]\")'><i class='fa fa-code'></i> open source code</a></div>" +
       '<div id="thegamma-[ID]-out" class="thegamma-out"><p class="placeholder">Loading the visualization...</p></div>' +      
@@ -79,7 +109,7 @@ function initTheGamma() {
       '  <div class="header"><a href="javascript:closeDialog(\'[ID]\');">&times;</a><span>Edit source code</span></div>' +
       '  <div class="body"><div id="thegamma-[ID]-ed"></div><div id="thegamma-[ID]-errors" class="errors"></div>' +
       '    <button id="thegamma-[ID]-update">Update page</button></div>' +
-      '</div></div>').replace(/\[ID\]/g, id);
+      '</div></div>').replace(/\[ID\]/g, info.id);
   });
   loadTheGamma();
 }
