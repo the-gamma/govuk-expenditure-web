60a35c5febaa69d2caed19b38191537fca4373a2
Tomas Petricek <tomas@tomasp.net>
2017-05-30 12:11:35 +0100

    Code cleanup, add some comments

--- web/lib/embed.js
+++ web/lib/embed.js
@@ -1,5 +1,7 @@
 if (!thegammaInit) { var thegammaInit = false; }
 
+// We're not using any framework here (to keep it self-contained), 
+// so the following implements simple dialog boxes for showing the code.
 function openDialog(id) {
   document.getElementById("thegamma-" + id + "-dialog").style.display="block";
   setTimeout(function() { 
@@ -15,13 +17,17 @@ function closeDialog(id) {
   },400)
 }
 
+// When page loads - initialize all The Gamma visualizations
 function loadTheGamma() {
   require.config({
     paths:{'vs':'node_modules/monaco-editor/min/vs'},
     map:{ "*":{"monaco":"vs/editor/editor.main"}}
   });
   require(["vs/editor/editor.main", "node_modules/thegamma-script/dist/thegamma.js"], function (_, g) {      
+    // Go over all the visualizations as defined by 'var thegamma = [ .. ]' in the index.html file
     thegamma.forEach(function (info) {
+      
+      // Define the providers available in the visualizations
       var id = info.id;
       var services = "https://thegamma-services.azurewebsites.net/";      
       var providers = 
@@ -55,17 +61,24 @@ function loadTheGamma() {
           } };
       var editor = ctx.createEditor("thegamma-" + id + "-ed", code, opts);
       
-      // Set source code, evalate it, 
+      // Set source code, evalate it and generate options for <select> elements
+      // (only when there are placeholders and 'info.editors' is specified)
       function setSource(code) {
         ctx.evaluate(code, "thegamma-" + id + "-out");      
         editor.setValue(code);
         if (info.editors) {
+          // Type check the source code & get result (as a JS promise)
           ctx.check(code).then(function(res) {
-            if (res.wellTyped) {          
+            if (res.wellTyped) {
+              // If type checking succeeded, get all 'syntactic entities' in the typed code,
+              // find all placeholders and generate <select> options for each placeholder
               res.getEntities()
                 .filter(function(ent) { return ent.kind == "placeholder"; })
                 .forEach(function(place) {
                   var html = "";
+                  // Get the name of the selected member and other possible members
+                  // e.g. given `foo.[p:bar]`, selected will be "bar" and members
+                  // will be all other members that are available in `foo.<here>`
                   var selected = place.getChildren()[0].getChildren()[1];
                   place.getChildren()[0].getChildren()[0].type.members
                     .filter(function(m) { return m != "preview"; })
@@ -73,9 +86,14 @@ function loadTheGamma() {
                       var sel = m == selected.name ? " selected" : "";
                       html += "<option value='" + m + "'" + sel + ">" + m + "</option>";
                     });
+                  
+                  // Set elements of the drop down. When selection changes,
+                  // replace the placeholder in the source code with a new one.
                   var drop = document.getElementById(info.editors + "-" + place.name);
                   drop.onchange = function() { 
-                    var newCode = code.substr(0, place.range.start) + "[" + place.name + ":'" + drop.value + "']" + code.substr(place.range.end + 1);
+                    var newCode = code.substr(0, place.range.start) + 
+                      "[" + place.name + ":'" + drop.value + "']" + 
+                      code.substr(place.range.end + 1);
                     setSource(newCode);
                   };
                   drop.innerHTML = html;
@@ -97,6 +115,7 @@ function loadTheGamma() {
   });
 }
 
+// Generate HTML for each dialog box
 function initTheGamma() {
   thegamma.forEach(function(info) {
     var el = document.getElementById(info.id);  
@@ -118,7 +137,7 @@ if (!thegammaInit) {
   thegammaInit=true; 
   var ol = window.onload;
   window.onload = function() { initTheGamma(); if (ol) ol(); };
-  var link = '<link href="https://thegamma.net/lib/thegamma/thegamma.css" rel="stylesheet">';
+  var link = '<link href="https://thegamma.net/lib/thegamma-0.1/thegamma.css" rel="stylesheet">';
   var heads = document.getElementsByTagName("head");
   if (heads.length > 0) heads[0].innerHTML += link;
   else document.write(link);
